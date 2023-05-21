a341a764453078654f242f81f6abb3e72f3a13ed
Slothy Boi <eric20wang.wang@gmail.com>
2023-05-21 15:57:54 -0700

    added prompt for char length part of password

--- Develop/script.js
+++ Develop/script.js
@@ -10,5 +10,25 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  //let browserText;
+  var inputLength = prompt("How many characters would you like your password to be?");
+  var validInputNotGiven = true;
+  while (validInputNotGiven) {
+    if (inputLength == null) {
+      alert("Please provide a desired password length");
+    } else if (isNaN(inputLength)) {
+      alert("Please choose a number");
+    } else if (inputLength < 8) {
+      alert("Please choose a password that is at least 8 characters long");
+    } else if (inputLength > 128) {
+      alert("Please choose a password that is no longer than 128 characters");
+    } else {
+      break;
+    }
+    var inputLength = prompt("How long would you like your password to be?");
+  }
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
